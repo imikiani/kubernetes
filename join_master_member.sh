ea0482f5a2a550a782e9973588f2d4abe259fe79
Imi Kiani <kianiman@gmail.com>
2021-01-15 23:31:16 +0330

    correct join commands

--- join_master_member.sh
+++ join_master_member.sh
@@ -3,8 +3,11 @@
 #LINE_NUMBER=$(echo $JOIN_COMMAND_OUTPUT | grep -n "You can now join any number of the control-plane" | cut -d: -f1)
 #NEXT_LINE_NUMBER=$[ $LINE_NUMBER + 1 ]
 
+# The first argument is ansible_host which passed to the script 
+APISERVER_ADVERTISE_ADDRESS=$1
+
 KUBEADM_INIT_OUTPUT_FILE=./token
 LINE_NUMBER=$(cat $KUBEADM_INIT_OUTPUT_FILE | grep -n "You can now join any number of the control-plane" | cut -d: -f1)
 
 COMMAND=`sed -n "$((LINE_NUMBER+2)),$((LINE_NUMBER+4))p" $KUBEADM_INIT_OUTPUT_FILE)`
-echo "$COMMAND --apiserver-advertise-address 172.16.16.102" 
+echo "$COMMAND --apiserver-advertise-address $APISERVER_ADVERTISE_ADDRESS" 
